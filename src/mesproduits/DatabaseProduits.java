7471e059563bac85f1cb32decf57e3372b57f3bf
JuluanQ <48562224+JuluanQ@users.noreply.github.com>
2021-10-12 21:22:00 +0200

    DatabaseProduits

---
+++ src/mesproduits/DatabaseProduits.java
@@ -0,0 +1,27 @@
+package mesproduits;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseProduits {
+
+    private List<Produit> listProduits;
+
+    public DatabaseProduits() {
+        this.listProduits = new ArrayList<>();
+    }
+
+    /** Rempli la liste de produit à l'aide d'une API
+
+     */
+    public List<Produit> makeDataBase(int nbProduits) {
+        for(int i = 0; i < nbProduits; i++){
+            //Ajout dans la liste
+        }
+        return null;
+    }
+
+    public List<Produit> getListProduits() {
+        return listProduits;
+    }
+}
