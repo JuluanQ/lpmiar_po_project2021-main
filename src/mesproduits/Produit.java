16717f48a676b75334143cad01e1b6e59add1d09
JuluanQ <48562224+JuluanQ@users.noreply.github.com>
2021-10-12 18:01:43 +0200

    oui

--- src/mesproduits/Produit.java
+++ src/mesproduits/Produit.java
@@ -4,29 +4,38 @@ import magasin.*;
 
 public class Produit implements iArticle{
 
-//    private int reference;
-//    private String nom;
-//    private double prix;
-//
-//
-//    public void nourriture(int reference, String nom, double prix){
-//        this.reference = reference;
-//        this.nom = nom;
-//        this.prix = prix;
-//    }
-//
-//    @Override
-//    public int reference() {
-//        return 0;
-//    }
-//
-//    @Override
-//    public String nom() {
-//        return nom;
-//    }
-//
-//    @Override
-//    public double prix() {
-//        return prix;
-//    }
+    private static int nbReference = 1;
+    private int reference;
+    private String nom;
+    private double prix;
+    private String description;
+
+    public Produit(String nom, double prix){
+        this.reference = nbReference++;
+        this.nom = nom;
+        this.prix = prix;
+    }
+
+    public Produit(String nom, String desc, double prix) {
+        this(nom, prix);
+        this.description = desc;
+    }
+
+    public Produit(String nom) {
+        this(nom, 0.0);
+    }
+
+    @Override
+    public int reference() {
+        return reference;
+    }
+    @Override
+    public String nom() {
+        return nom;
+    }
+
+    @Override
+    public double prix() {
+        return prix;
+    }
 }
