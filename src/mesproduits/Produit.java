ed4d755621e9fcd2d1069269e43b1376aec1e725
Nolwennclrc <nolwenn.cloarec@etu.univ-nantes.fr>
2021-10-12 10:55:04 +0200

    packages mesproduits

---
+++ src/mesproduits/Produit.java
@@ -0,0 +1,32 @@
+package mesproduits;
+import magasin.*;
+
+
+public class Produit implements iArticle{
+
+//    private int reference;
+//    private String nom;
+//    private double prix;
+//
+//
+//    public void nourriture(int reference, String nom, double prix){
+//        this.reference = reference;
+//        this.nom = nom;
+//        this.prix = prix;
+//    }
+//
+//    @Override
+//    public int reference() {
+//        return 0;
+//    }
+//
+//    @Override
+//    public String nom() {
+//        return nom;
+//    }
+//
+//    @Override
+//    public double prix() {
+//        return prix;
+//    }
+}
