d35b9872096cbee707f10eff62c7079b83d75957
E183247G <E183247G@etu.univ-nantes.fr>
2021-10-13 15:30:18 +0200

    Debut d'appli fonctionnelle

--- src/monapplication/MonApplication.java
+++ src/monapplication/MonApplication.java
@@ -3,6 +3,7 @@ package monapplication;
 import magasin.*;
 import magasin.exceptions.ArticleDejaEnStockException;
 import magasin.exceptions.QuantiteNegativeException;
+import mesproduits.Client;
 import mesproduits.DatabaseProduits;
 import mesproduits.Produit;
 
@@ -36,31 +37,69 @@ public class MonApplication {
         System.out.println("client : devenir un client\n" +
                 "stock : consulte les stock du magasin\n" +
                 "ajoutPanier : ajouter un article au panier\n" +
-                "consultPanier : consulter son panier");
+                "consultPanier : consulter son panier\n" +
+                "help : donne l'ensemble des commandes disponibles\n" +
+                "quit : quitter l'application");
         System.out.printf("Veuillez écrire ce que vous souhaitez faire : ");
         String cmd = sc.next();
+        Client client = new Client();
 
-        switch (cmd){
-            case "client":
-                //magasin.enregistrerNouveauClient(new );
-                break;
-            case "stock":
-                System.out.println(magasin.listerArticlesEnStockParNom());
-                break;
-            case "ajoutPanier":
-                System.out.printf("Quel est le nom du produit que vous souhaitez ajouter ?");
-                String nomProduit = sc.next();
-                System.out.printf("Quel quantité souhaitez vous ajouter ?");
-                int nbProduit = sc.nextInt();
-                Produit produit = new Produit(nomProduit);
-                //magasin.ajouterAuPanier(,produit,nbProduit);
-                break;
-            case "consultPanier":
-                System.out.println("PANIER :");
-                //magasin.consulterPanier();
-                break;
-            default:
-                System.out.println(":(");
+        boolean quit = false;
+
+        while(!quit){
+            switch (cmd){
+                case "client":
+                    try{
+                        magasin.enregistrerNouveauClient(client);
+                    }catch (Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case "stock":
+                    System.out.println(magasin.listerArticlesEnStockParNom());
+                    break;
+                case "ajoutPanier":
+                    System.out.printf("Quel est le nom du produit que vous souhaitez ajouter ? : ");
+                    String nomProduit = sc.next();
+                    System.out.printf("Quel quantité souhaitez vous ajouter ? : ");
+                    int nbProduit = sc.nextInt();
+                    Produit produit = new Produit(nomProduit);
+                    try{
+                        for(Produit p : databaseProduits.getListProduits()){
+                            if(p.nom().equals(nomProduit)){
+                                magasin.ajouterAuPanier(client,p,nbProduit);
+                            }
+                        }
+                    }catch (Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case "consultPanier":
+                    System.out.println("PANIER :");
+                    try{
+                        System.out.println(magasin.consulterPanier(client).listerArticlesParNom());
+                    }catch(Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case "help":
+                    System.out.println("client : devenir un client\n" +
+                            "stock : consulte les stock du magasin\n" +
+                            "ajoutPanier : ajouter un article au panier\n" +
+                            "consultPanier : consulter son panier\n" +
+                            "help : donne l'ensemble des commandes disponibles\n" +
+                            "quit : quitter l'application");
+                    break;
+                case "quit":
+                    quit = true;
+                    break;
+                default:
+                    System.out.println(":(");
+            }
+            if(!quit){
+                System.out.printf("Veuillez écrire ce que vous souhaitez faire : ");
+                cmd = sc.next();
+            }
         }
     }
 }
